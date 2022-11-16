840234ca3a14fe0e468a6d809b477e291dbbc2de
Hendra Juli <hendrjl.devs@gmail.com>
2022-11-16 22:00:48 +0800

    cleaning About page

--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,6 +1,12 @@
+import { Container, Row, Card, Text, } from "@nextui-org/react";
+
 const About = () => {
   return (
-    <h1>ASD</h1>
+    <Row justify="center" align="center">
+      <Text h6 size={15} color="white" css={{ m: 0 }}>
+        <h1>About Page</h1>
+      </Text>
+    </Row>
   )
 }
 
